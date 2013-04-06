034cd4fb7a0527dfbbf06f6830ddf0b58337d858
Balazs Ree <ree@greenfinity.hu>
2013-04-06 13:07:52 +0200

    progress tests

--- test/collection-test.js
+++ test/collection-test.js
@@ -43,6 +43,41 @@ buster.testCase('grunt-collection-helper', {
                 assert.equals(c.base, '.');
                 assert.equals(c.config, {});
             }
+        },
+        'select': {
+            'setUp': function () {
+                 var config1 = {
+                    'a.js': ['a1.js', 'a2.js'],
+                    'b.js': ['b1.js', 'b2.js'],
+                    'wrong': 'NOTANARRAY'
+                };
+                this.mockFile.expects('readJSON')
+                    .returns(config1);
+                this.c = collection.local().collection;
+            },
+            'finds key': function () {
+                assert.equals(this.c.select('a.js'), ['a1.js', 'a2.js']);
+                assert.equals(this.c.select('b.js'), ['b1.js', 'b2.js']);
+            },
+            'nonexistent key yields error': function () {
+                var c = this.c;
+                assert.exception(function () {
+                    c.select('NOSUCH');
+                }, 'Error');
+            },
+            'non-list value yields error': function () {
+                var c = this.c;
+                assert.exception(function () {
+                    c.select('wrong');
+                }, 'Error');
+            },
+            'non-string key yields error': function () {
+                // This feature will be implemented at some point.
+                var c = this.c;
+                assert.exception(function () {
+                    c.select({features: ['foo', 'bar']});
+                }, 'Error');
+            }
         }
     }
 });
